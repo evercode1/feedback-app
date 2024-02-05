7a4053c0bb9bb462cc44c9b257672edc3d892b20
evercode1 <ikon321@yahoo.com>
2024-02-04 20:30:45 -0800

    animate add and delete

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,23 +1,55 @@
+import { motion, AnimatePresence } from 'framer-motion'
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
 
 function FeedbackList({feedback, handleDelete}) {
 
-
   return (
 
     
     <div className="feeback-list">
 
+      <AnimatePresence>
+
       {feedback.map((item) => (
 
-          <FeedbackItem key={item.id} item ={item} handleDelete={handleDelete} />
+        <motion.div 
+        
+          key={item.id}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          
+        >
+
+            <FeedbackItem key={item.id} item ={item} handleDelete={handleDelete} />
+
+        </motion.div>
       
       ))}
 
+      
+
+      </AnimatePresence>
+
     </div>
   )
+
+
+  // return (
+
+    
+  //   <div className="feeback-list">
+
+  //     {feedback.map((item) => (
+
+  //         <FeedbackItem key={item.id} item ={item} handleDelete={handleDelete} />
+      
+  //     ))}
+
+  //   </div>
+  // )
 }
 
 FeedbackList.proptypes = {
