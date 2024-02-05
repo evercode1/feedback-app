9ed0c95897383e79993ed86e54f7f85adf053eea
evercode1 <ikon321@yahoo.com>
2024-02-04 17:18:11 -0800

    form validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,8 +5,26 @@ import Button from './shared/Button'
 function FeedbackForm() {
 
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {  
+
+        if (text === '') {
+
+            setBtnDisabled(true)
+            setMessage(null)
+
+        } else if (text !== '' && text.trim().length <= 10) {
+
+            setBtnDisabled(false)
+            setMessage('Your review must be at least 10 characters long')
+            
+        } else {
+
+            setBtnDisabled(false)
+            setMessage(null)
+        }
         
         setText(e.target.value)
 
@@ -26,9 +44,11 @@ function FeedbackForm() {
 
                 <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text} />
 
-                <Button type='submit'>Send</Button>
+                <Button type='submit' isDisabled={btnDisabled}>Send</Button>
 
             </div>
+
+            {message && <div className="message">{message}</div>}
 
 
         </form>
